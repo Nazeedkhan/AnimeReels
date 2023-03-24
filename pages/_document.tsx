9f7232d9b92699da9abe23d52a6b8d9d75a99449
Nazeed khan <nazeedkhan10@gmail.com>
2023-03-24 14:07:47 +0530

    done

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import favicon from '../public/favicon.webp'
+import { Html, Main, NextScript, Head } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
-      <title>AnimeReels❤️</title>
-      <link rel="shortcut icon" href='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfx3C2DMJOA_hYkBB78wO9IaDgDcd268rLoiWzv9Bal3NXQg30i9K1Zb2X4GXQX4LOzOw&usqp=CAU' />
+      <Head>
+        <title>AnimeReels❤️</title>
+        <link
+          rel="shortcut icon"
+          href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfx3C2DMJOA_hYkBB78wO9IaDgDcd268rLoiWzv9Bal3NXQg30i9K1Zb2X4GXQX4LOzOw&usqp=CAU"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
