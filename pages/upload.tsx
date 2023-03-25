bd7155735eadccbfde41fc2895105295f1e023ba
Nazeed khan <nazeedkhan10@gmail.com>
2023-03-25 12:26:34 +0530

    some changes

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { FaCloudUploadAlt } from "react-icons/fa";
-import { MdDelete } from "react-icons/md";
 import axios from "axios";
 import useAuthStore from "@/store/authStore";
 import { client } from "@/utils/client";
